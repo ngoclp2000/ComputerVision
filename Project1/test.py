93feb085525b781d776ba9d686cae366fcb428cb
meomeocoj <tminh1103@gmail.com>
2023-02-13 20:55:44 +0700

    modified: logic detect corners

--- Project1/test.py
+++ Project1/test.py
@@ -3,7 +3,7 @@ import numpy as np
 
 max_size = (20, 20)
 
-img = cv2.imread('preprocess_img/Chessboard_0541.png')
+img = cv2.imread('preprocess_img/Chessboard0631.png')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Apply Gaussian Blur to remove noise
@@ -24,20 +24,22 @@ canvas = cv2.flip(canvas, 1)
 
 th, im_th_tz = cv2.threshold(canvas, canvas.mean() * 0.975, 255, cv2.THRESH_TOZERO)
 
-for rows in range(3, max_size[0]+1):
-    for cols in range(3, max_size[1]+1):
+result = (0,0) 
+ret = None
+corners = None
+
+for rows in range(max_size[0]+1,3,-1):
+    for cols in range(max_size[1]+1,3,-1):
+        if rows > cols :
+             continue
         # Try to detect the corners of the chessboard with the current size
         ret, corners = cv2.findChessboardCorners(im_th_tz, (rows, cols), None)
         
         if ret:
             # Chessboard found with the current size, log the size and break from the loops
-            chessboard_size = (cols, rows)
-
-if not ret:
-    print("Chessboard size:", chessboard_size)
-
-# ret, corners = cv2.findChessboardCorners(im_th_tz, (11, 7), None)
-# sth = cv2.drawChessboardCorners(im_th_tz, (11, 7), corners, ret)
-
-# cv2.imshow("sth", sth)
-# cv2.waitKey(10000)
+            result = (rows, cols)
+            print("Chessboard size:", result)
+            break;
+    if ret:
+        break
+cv2.drawChessboardCorners(img, result, corners, ret)
